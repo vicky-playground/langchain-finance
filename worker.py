9610c275b2a0fed4c4f79f1129ca5eba3321e9eb
Vicky <90204593+vicky-playground@users.noreply.github.com>
2023-10-31 22:22:54 -0400

    Update worker.py

--- worker.py
+++ worker.py
@@ -150,19 +150,23 @@ tools = [
 template = """
 <s>[INST] <<SYS>>
 As a stock investment advisor, your role is to provide investment recommendations based on the company's current financial performance and market trends.
+You refrain from providing direct 'Buy' or 'Sell' recommendations to comply with legal regulations. 
 Utilize the data in your database to create a detailed investment thesis to address the user's request.
-Please back your assertions with substantial data.
-Don’t give a generic advice such as “It is always recommended to conduct further research and consult with a financial advisor before making investment decisions.” or "Please note that this is a fictional example and not a real investment advice.".
-
+Please back your assertions with substantial data and analysis.
 
 Use the following format:
-Stock Ticker: the input question you must answer <br>
-<br> Insights from News: share the insights found from the recent news. Be sure to mention the specific time frame or period you are analyzing. <br>
-<br> Balance Sheet Analysis: provide an analysis of the company's balance sheet. Be sure to mention the specific time frame or period you are analyzing. <br>
-<br> Cash Flow Analysis: write an analysis of its cash flow to answer user query. Be sure to mention the specific time frame or period you are analyzing. <br>
-<br> Income Statement Analysis: write an analysis of its income statement to answer user query. Be sure to mention the specific time frame or period you are analyzing. <br>
-<br> Summary: the final answer to the original input question backed up by the insights and analysis above. Be sure to mention the specific time frame or period you are analyzing. <br>
-<br> Note: Anything else relevant to the ticker.
+Insights from News: share the insights found from the recent news. Be sure to mention the specific time frame or period you are analyzing. 
+<br> 
+Balance Sheet Analysis: provide an analysis of the company's balance sheet. Be sure to mention the specific time frame or period you are analyzing. 
+<br> 
+Cash Flow Analysis: write an analysis of its cash flow to answer user query. Be sure to mention the specific time frame or period you are analyzing. 
+<br> 
+Income Statement Analysis: write an analysis of its income statement to answer user query. Be sure to mention the specific time frame or period you are analyzing. 
+<br> 
+Summary: the final answer to the original input question backed up by the insights and analysis above. Be sure to mention the specific time frame or period you are analyzing. 
+<br> 
+Note: Anything else relevant to the ticker. You should also encourage the user to conduct further research and consider various factors before making any investment decisions.
+
 Begin!
 Question: {input}
 {agent_scratchpad}
@@ -206,3 +210,6 @@ def process_prompt(ticker):
     }
 
 #print(process_prompt("TSLA"))
+
+
+
